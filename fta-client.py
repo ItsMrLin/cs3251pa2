09337f8e878beb9d6c2ab5c4e72130e2ab91018f
Pav <pav920@gmail.com>
2014-11-26 18:55:47 -0500

    added basic command line arguments to fta server and client. working on client commands now

--- fta-client.py
+++ fta-client.py
@@ -1,6 +1,9 @@
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument("square", help="display a square of a given number",
-                    type=int)
+parser.add_argument("X", help=" the port number at which the fta-client’s UDP socket should bind to (even number). Please remember that this port number should be equal to the server’s port number minus 1.", type=int)
+parser.add_argument("A", help="the IP address of NetEmu")
+parser.add_argument("P", help="the UDP port number of NetEmu", type=int)
 args = parser.parse_args()
-print args.square**2
+print args.X
+print args.A
+print args.P
--- fta-server.py
+++ fta-server.py
@@ -1,6 +1,9 @@
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument("square", help="display a square of a given number",
-                    type=int)
+parser.add_argument("X", help="the port number at which the fta-server’s UDP socket should bind to (odd number)", type=int)
+parser.add_argument("A", help="the IP address of NetEmu")
+parser.add_argument("P", help="the UDP port number of NetEmu", type=int)
 args = parser.parse_args()
-print args.square**2
+print args.X
+print args.A
+print args.P
