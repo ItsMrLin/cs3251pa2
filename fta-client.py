bff9c679e415fbdc3348a803babaaf16525477f2
Pav <pav920@gmail.com>
2014-11-26 19:02:07 -0500

    multiple commands for fta client work now

--- fta-client.py
+++ fta-client.py
@@ -1,9 +1,11 @@
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument("X", help=" the port number at which the fta-client’s UDP socket should bind to (even number). Please remember that this port number should be equal to the server’s port number minus 1.", type=int)
+parser.add_argument("X", type=int)
 parser.add_argument("A", help="the IP address of NetEmu")
 parser.add_argument("P", help="the UDP port number of NetEmu", type=int)
+parser.add_argument('commands',nargs='*', help='list of commands.')
 args = parser.parse_args()
 print args.X
 print args.A
 print args.P
+print args.commands
